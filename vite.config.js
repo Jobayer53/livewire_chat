7a15262c7a2bf0d0f67d2550d31e1bcd674cf369
Jobayer Shikder <mdjobayer.h53@gmail.com>
2025-05-03 11:55:52 +0600

    ff

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/build/', 
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        assetsDir: 'build/assets', // Make sure images are bundled correctly
+    },
 });
