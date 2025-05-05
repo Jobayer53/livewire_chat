da367a2c669db0f39442f539dcdd51fe694a07c1
Jobayer Shikder <mdjobayer.h53@gmail.com>
2025-05-05 18:30:24 +0600

    gg

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,8 @@ $watch('darkTheme', value => {
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Chat </title>
     <link rel="shortcut icon" href="{{ asset('frontend/assets/images/favicon.ico') }}">
