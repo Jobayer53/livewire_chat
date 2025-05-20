248344a5ff6179e9cc55305c1ddd0bee4a4bf640
Jobayer Shikder <mdjobayer.h53@gmail.com>
2025-05-20 11:02:15 +0600

    optimized

--- app/Console/Commands/MarkInactiveUsers.php
+++ app/Console/Commands/MarkInactiveUsers.php
@@ -27,17 +27,23 @@ class MarkInactiveUsers extends Command
      */
     public function handle()
     {
-         $threshold = now()->subMinutes(5);
+        $threshold = now()->subMinutes(5);
+        DB::table('users')
+            ->whereNotExists(function ($query) use ($threshold) {
+                $query->select(DB::raw(1))
+                    ->from('sessions')
+                    ->whereRaw('sessions.user_id = users.id')
+                    ->where('sessions.last_activity', '>=', $threshold->timestamp);
+            })->update(['is_online' => 0]);
+        // DB::table('sessions')
+        //     ->where('last_activity', '<', $threshold->timestamp)
+        //     ->pluck('user_id')
+        //     ->unique()
+        //     ->filter()
+        //     ->each(function ($userId) {
+        //         User::where('id', $userId)->update(['is_online' => 0]);
+        //     });
 
-    DB::table('sessions')
-        ->where('last_activity', '<', $threshold->timestamp)
-        ->pluck('user_id')
-        ->unique()
-        ->filter()
-        ->each(function ($userId) {
-            User::where('id', $userId)->update(['is_online' => 0]);
-        });
-
-    $this->info('Inactive users updated.');
+        $this->info('Inactive users updated.');
     }
 }
